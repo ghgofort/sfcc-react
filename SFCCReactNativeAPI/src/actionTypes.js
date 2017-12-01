e09836be594f74f2e02d76557102d4072a2b7d0d
Galen Goforth <galengoforth@gmail.com>
2017-12-01 11:18:46 -0700

    Removed old project files from before implementing create-react-native-app

--- SFCCReactNativeAPI/src/actionTypes.js
+++
@@ -1,8 +0,0 @@
-/**
- * actionTypes.js
- * Define action types for non-navigation actions that need to be emitted
- * in order to update the state of the application.
- */
-
-/***** OTHER ACTION TYPES **********/
-export const SCENE_TRANSITION = 'sceneTransition';
