b8eb748cbf5f4d3096d4f0002b0376c3bfee82a2
Galen Goforth <galengoforth@gmail.com>
2018-02-09 17:23:11 -0700

    Setup Mobile App v_1.0.0 : ##Added:## - Stateless function component for MenuCatagory (MenuCatagory.js) to display individual product catagories in the hamburger menu recursively.
    ##Changed:##
    - ProductCatagoryTree.js : Modified the component to use the MenuCatagory component for rendering all of the individual components.

--- SFCC-RN-Components/src/components/Navbar/ProductCategoryTree.js
+++ SFCC-RN-Components/src/components/Navbar/ProductCategoryTree.js
@@ -1,6 +1,14 @@
+/**
+ * @file ProductCategoryTree.js
+ * The ProductCatagoryTree file is a React Native component used as part of the
+ * Navbar component, but could be adopted for use in other places in the app if
+ * needed.
+ */
+
 import React, { Component } from 'react';
 import {  View, Text, } from 'react-native';
 import PropTypes from 'prop-types';
+import { MenuCatagory } from "./MenuCatagory";
 
 export default class ProductCategoryTree extends Component {
   constructor(props) {
@@ -12,7 +20,22 @@ export default class ProductCategoryTree extends Component {
     };
   };
 
+  _renderCatagory() {
+
+  }
+
+  /**
+   * Loops through each top level catagory, and then recursivly calls renderCatagory
+   * to fill in all of the child catagories.
+   */
+  _renderCatagories() {
+    const cats = this.state.catagories;
+
+  }
+
   render() {
+    const cats = this._renderCatagories();
+
     return (
       <View>
         <Text> </Text>
@@ -24,3 +47,5 @@ export default class ProductCategoryTree extends Component {
 ProductCategoryTree.propTypes = {
   catagories: PropTypes.array
 };
+
+
